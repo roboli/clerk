2f4348aad314bcb4b37c551c65f125b6f9450b54
Roberto Oliveros <robertooliveros@mac.com>
2014-12-10 12:15:27 -0600

    Testing with cljs-build.

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [om "0.7.3"]
+                 [com.facebook/react "0.11.2"]
                  [secretary "1.2.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [com.cemerick/clojurescript.test "0.3.1"]]
 
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
-                             [com.cemerick/clojurescript.test "0.3.1"]]}}
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]]}}
 
   :source-paths ["src/clj" "src/cljs"]
 
@@ -22,6 +23,13 @@
                 :output-dir "out"
                 :optimizations :none
                 :source-map true}}
+
+             {:id "test"
+              :source-paths ["src/cljs" "test/cljs"]
+              :compiler {
+                         :preamble ["react/react.js"]
+                         :output-to "test.js"
+                         :optimizations :whitespace}}
 
              ;; examples
              {:id "greeting"
@@ -38,4 +46,6 @@
                          :output-to "examples/simple-site/app.js"
                          :output-dir "examples/simple-site/out"
                          :source-map "examples/simple-site/app.js.map"
-                         :optimizations :none}}]})
+                         :optimizations :none}}]
+
+              :test-commands {"unit-tests" ["phantomjs" :runner "test/vendor/phantomjs-shims.js" "test.js"]}})
